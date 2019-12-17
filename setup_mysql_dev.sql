f2cad94131d111339650824a6277d7165fe6454d
Jose Calderon <jose.calderon@holbertonschool.com>
2019-12-17 13:22:32 -0500

    [FIX]

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,6 +1,12 @@
+-- Create the database hbnb_dev_db
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+-- Use the previous database
 USE hbnb_dev_db;
+-- Create the specific user to this database
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+-- Give the priviliges to this database to the user
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+-- Use other database
 USE performance_schema;
+-- Give privileges to this other database.
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
