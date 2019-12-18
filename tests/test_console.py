f8c328eb2d257a09e98d67fda06c9d0bd73d0942
Jose Calderon <jose.calderon@holbertonschool.com>
2019-12-18 17:47:11 -0500

    [UPDATE]

--- tests/test_console.py
+++ tests/test_console.py
@@ -89,6 +89,43 @@ class TestConsole(unittest.TestCase):
             self.assertEqual(
                 "[[User]", f.getvalue()[:7])
 
+    def test_create_v2(self):
+        """Test create command input"""
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.consol.onecmd(
+                "create City name=\"Las_Vegas\""
+            )
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.consol.onecmd(
+                "City.all()"
+            )
+            self.assertTrue(
+                "'Las Vegas'" in f.getvalue()
+            )
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.consol.onecmd(
+                "create Place city_id=\"0001\" \
+user_id=\"0001\" name=\"My_little_house\" number_rooms=4 \
+number_bathrooms=2 max_guest=10 price_by_night=300 \
+                latitude=37,773972 longitude=-122,431297"
+            )
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.consol.onecmd(
+                "Place.all()"
+            )
+            s = f.getvalue()
+            self.assertTrue(
+                "'longitude': -122.431297" in s and
+                "'latitude': 37.773972" in s and
+                "'price_by_night': 300" in s and
+                "'max_guest': 10" in s and
+                "'number_bathrooms': 2" in s and
+                "'number_rooms': 4" in s and
+                "'name': 'My little house'" in s and
+                "'user_id': '0001'" in s and
+                "'city_id': '0001'" in s
+            )
+
     def test_show(self):
         """Test show command inpout"""
         with patch('sys.stdout', new=StringIO()) as f:
