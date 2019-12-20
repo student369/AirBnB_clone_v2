a190ccb29ff420f9bf4fb7a9704ae47b22613fb1
Jose Calderon <jose.calderon@holbertonschool.com>
2019-12-20 16:28:19 -0500

    [UPDATE]

--- models/user.py
+++ models/user.py
@@ -1,33 +1,31 @@
 #!/usr/bin/python3
-"""This is the user class"""
-from models.base_model import BaseModel
-from models.place import Place
-from models.review import Review
-from sqlalchemy.ext.declarative import declarative_base
+'''
+    Implementation of the User class which inherits from BaseModel
+'''
+from models.base_model import BaseModel, Base
+import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-
-Base = declarative_base()
+from os import getenv
 
 
 class User(BaseModel, Base):
-    """This is the class for user
-    Attributes:
-        email: email address
-        password: password for you login
-        first_name: first name
-        last_name: last name
-    """
+    '''
+        Definition of the User class
+    '''
     __tablename__ = "users"
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=False)
-    last_name = Column(String(128), nullable=False)
-    places = relationship(
-        "Place", cascade="all, delete",
-        backref="user"
-    )
-    reviews = relationship(
-        "Review", cascade="all, delete",
-        backref="user"
-    )
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+
+        places = relationship('Place', cascade='all, delete-orphan',
+                              backref='user')
+        reviews = relationship('Review', cascade='all, delete-orphan',
+                               backref='user')
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
