da5a2720a98de08754efaa76f8d5eb2c98a2add0
Jose Calderon <jose.calderon@holbertonschool.com>
2019-12-20 16:35:08 -0500

    [UPDATE]

--- models/city.py
+++ models/city.py
@@ -1,27 +1,23 @@
 #!/usr/bin/python3
-"""This is the city class"""
-from models.base_model import BaseModel
-from models.place import Place
-from sqlalchemy.ext.declarative import declarative_base
+'''
+    Define the class City.
+'''
+from models.base_model import BaseModel, Base
+import sqlalchemy
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
-
-Base = declarative_base()
+from os import getenv
 
 
 class City(BaseModel, Base):
-    """This is the class for City
-    Attributes:
-        state_id: The state id
-        name: input name
-    """
-    __tablename__ = "cities"
-    name = Column(String(128), nullable=False)
-    state_id = Column(
-        String(60), ForeignKey("states.id"),
-        nullable=False
-    )
-    places = relationship(
-        "Place", cascade="all, delete",
-        backref="cities"
-    )
+    '''
+        Define the class City that inherits from BaseModel.
+    '''
+    __tablename__ = 'cities'
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        places = relationship('Place', cascade='all, delete', backref='city')
+    else:
+        state_id = ""
+        name = ""
