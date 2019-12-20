48c8693d2b7b4cb3f2a2b1f3f17c0d59112543b4
Jose Calderon <jose.calderon@holbertonschool.com>
2019-12-19 19:10:59 -0500

    [UPDATE]

--- models/amenity.py
+++ models/amenity.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
 """This is the amenity class"""
 from models.base_model import BaseModel
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Table, Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+from models import place_amenity
+
+Base = declarative_base()
 
 
-class Amenity(BaseModel):
+class Amenity(BaseModel, Base):
     """This is the class for Amenity
     Attributes:
         name: input name
     """
-    name = ""
+    __tablename__ = "amenities"
+    name = Column(String(128), nullable=False)
