75f85ee1788b00c399ee85e455c7a58ed32fc398
Jose Calderon <jose.calderon@holbertonschool.com>
2019-12-20 16:28:08 -0500

    [UPDATE]

--- models/review.py
+++ models/review.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python3
-"""This is the review class"""
-from models.base_model import BaseModel
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, ForeignKey
+'''
+    Implementation of the Review class
+'''
 
-Base = declarative_base()
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from os import getenv
 
 
 class Review(BaseModel, Base):
-    """This is the class for Review
-    Attributes:
-        place_id: place id
-        user_id: user id
-        text: review description
-    """
-    __tablename__ = "reviews"
-    text = Column(String(1024), nullable=False)
-    place_id = Column(
-        String(60), ForeignKey("places.id"), nullable=False
-    )
-    user_id = Column(
-        String(60), ForeignKey("users.id"), nullable=False
-    )
+    '''
+        Implementation for the Review.
+    '''
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        __tablename__ = "reviews"
+        text = Column(String(1024), nullable=False)
+        place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
+        user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+    else:
+        place_id = ""
+        user_id = ""
+        text = ""
