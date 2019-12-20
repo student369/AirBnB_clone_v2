53f9ca35038d76043e0d1e2b0b6bcd5d95bdc1f4
Jose Calderon <jose.calderon@holbertonschool.com>
2019-12-19 19:13:48 -0500

    [UPDATE]

--- models/state.py
+++ models/state.py
@@ -1,11 +1,34 @@
 #!/usr/bin/python3
 """This is the state class"""
+import os
+import models
+from models import City, State
 from models.base_model import BaseModel
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
+Base = declarative_base()
+HBNB_TYPE_STORAGE = os.getenv("HBNB_TYPE_STORAGE")
+
+
+class State(BaseModel, Base):
     """This is the class for State
     Attributes:
         name: input name
     """
-    name = ""
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+
+    if HBNB_TYPE_STORAGE == "db":
+        cities = relationship(
+            "City", cascade="all, delete",
+            backref="state"
+        )
+    elif HBNB_TYPE_STORAGE == "file":
+        ct = {}
+        for k, v in models.storage.all(City).items():
+            if v.state_id == State.id:
+                ct[k] = v
+        cities = ct
