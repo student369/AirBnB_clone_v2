051872151a1f45a8504ff929ceb748c36bf6fbfc
Jose Calderon <jose.calderon@holbertonschool.com>
2019-12-19 19:09:11 -0500

    [UPDATE]

--- models/place.py
+++ models/place.py
@@ -1,13 +1,29 @@
 #!/usr/bin/python3
 """This is the place class"""
+import os
+import models
 from models.base_model import BaseModel
-from models import Place
+from models import Place, Amenity, Review
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, ForeignKey, Float
+from sqlalchemy import Table, Column, Integer, String, ForeignKey, Float
 from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 HBNB_TYPE_STORAGE = os.getenv("HBNB_TYPE_STORAGE")
+
+place_amenity = Table(
+    "place_amenity", Base.metadata,
+    Column(
+        "place_id", String(60),
+        ForeignKey("Place.id"), nullable=False,
+        primary_key=True
+    ),
+    Column(
+        "amenity_id", String(60),
+        ForeignKey("Amenity.id"), nullable=False,
+        primary_key=True
+    )
+)
 
 
 class Place(BaseModel):
@@ -58,9 +74,31 @@ class Place(BaseModel):
             "Review", cascade="all, delete",
             backref="place"
         )
+        amenities = relationship(
+            "Amenity", secondary=place_amenity,
+            view_only=False
+        )
     elif HBNB_TYPE_STORAGE == "file":
-        ct = {}
-        for k, v in models.storage.all(Review).items():            
-            if v.place_id == Place.id:
-                ct[k] = v
-        reviews = ct
+
+        @property
+        def reviews(self):
+            """Review:list Returns a list of Reviews"""
+            lst = []
+            for k, v in models.storage.all(Review).items():
+                if v.place_id == Place.id:
+                    lst.append(v)
+            return (lst)
+
+        @property
+        def amenities(self):
+            """Amenity:dict Returns the amenities"""
+            lst = []
+            for k, v in models.storage.all(Amenity).items():
+                if v.id in self.amenity_ids:
+                    lst.append(v)
+            return (lst)
+
+        @amenities.setter
+        def amenities(self, value):
+            if isinstance(value, Amenity):
+                self.amenity_ids.append(value.id)
