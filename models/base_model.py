626e6768dedd33ee02de107306235e6f8c9f5884
Jose Calderon <jose.calderon@holbertonschool.com>
2019-12-19 19:11:49 -0500

    [UPDATE]

--- models/base_model.py
+++ models/base_model.py
@@ -3,12 +3,25 @@
 import uuid
 import models
 from datetime import datetime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, DateTime
+
+Base = declarative_base()
 
 
 class BaseModel:
     """This class will defines all common attributes/methods
     for other classes
     """
+    id = Column(String(60), nullable=False, primary_key=True)
+    created_at = Column(
+        DateTime, nullable=False,
+        default=datetime.utcnow()
+    )
+    updated_at = Column(
+        DateTime, nullable=False,
+        default=datetime.utcnow()
+    )
 
     def __init__(self, *args, **kwargs):
         """Instantiation of base model class
@@ -29,7 +42,6 @@ class BaseModel:
         else:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
-            models.storage.new(self)
 
     def __str__(self):
         """returns a string
@@ -48,6 +60,7 @@ class BaseModel:
         """updates the public instance attribute updated_at to current
         """
         self.updated_at = datetime.now()
+        models.storage.new(self)
         models.storage.save()
 
     def to_dict(self):
@@ -56,7 +69,14 @@ class BaseModel:
             returns a dictionary of all the key values in __dict__
         """
         my_dict = dict(self.__dict__)
+        if "_sa_instance_state" in my_dict:
+            del my_dict["_sa_instance_state"]
         my_dict["__class__"] = str(type(self).__name__)
         my_dict["created_at"] = self.created_at.isoformat()
         my_dict["updated_at"] = self.updated_at.isoformat()
         return my_dict
+
+    def delete(self):
+        """deletes the current instance
+        """
+        models.storage.save(self)
