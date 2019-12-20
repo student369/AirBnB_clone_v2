3e1a17053ee49dca37ee1f2286182f52834e9e91
Jose Calderon <jose.calderon@holbertonschool.com>
2019-12-20 16:27:27 -0500

    [UPDATE]

--- models/amenity.py
+++ models/amenity.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
-"""This is the amenity class"""
-from models.base_model import BaseModel
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Table, Column, String, ForeignKey
+'''
+    Implementation of the Amenity class
+'''
+from models.base_model import BaseModel, Base
+from os import getenv
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
-Base = declarative_base()
-
-
 class Amenity(BaseModel, Base):
-    """This is the class for Amenity
-    Attributes:
-        name: input name
-    """
-    __tablename__ = "amenities"
-    name = Column(String(128), nullable=False)
+    '''
+        Implementation for the Amenities.
+    '''
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        __tablename__ = "amenities"
+        name = Column(String(128), nullable=False)
+    else:
+        name = ""
